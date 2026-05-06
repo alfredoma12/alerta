1dca0b1ac3f04a6551b86ce3c68f5c772e9cfd41
cualquera <alfredomunoz0696@gmail.com>
2026-05-06 17:26:13 -0400

    front: use VITE_API_URL and set ngrok API for development

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = 'https://tuesday-reproach-crestless.ngrok-free.dev'
+const API = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
 
 export const api = axios.create({
   baseURL: API,
