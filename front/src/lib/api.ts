031ce475ec4a4437bf03e7a5121456e8af4e17a5
alfredo <alfredomunoz0696@gmail.com>
2026-05-06 15:12:42 -0400

    fix: avoid ngrok CORS interstitial in frontend requests

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -4,5 +4,8 @@ const API = 'https://tuesday-reproach-crestless.ngrok-free.dev'
 
 export const api = axios.create({
   baseURL: API,
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+  },
 })
 
